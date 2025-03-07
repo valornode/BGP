0ed1e3dbbb67805f8c3f426f311595cc5d6e863e
valornode <support@valornode.net>
2025-03-07 12:07:01 -0600

    Update install-basics.sh

--- install-basics.sh
+++ install-basics.sh
@@ -12,12 +12,10 @@ echo "Installing Git..."
 sudo apt install -y git
 
 echo "Installing bgpq4..."
-git clone https://github.com/bgp/bgpq4.git /tmp/bgpq4
-cd /tmp/bgpq4
-make
-sudo make install
-cd ~
-rm -rf /tmp/bgpq4
+sudo apt update
+sudo apt install -y snapd
+sudo snap install snapd
+sudo snap install bgpq4 --edge
 
 echo "Installing BIRD..."
 sudo apt install -y bird2
@@ -34,6 +32,16 @@ sudo apt install -y pathvector
 echo "Enabling IPv4 forwarding..."
 echo "net.ipv4.ip_forward=1" | sudo tee /etc/sysctl.d/99-ipv4-forwarding.conf > /dev/null
 sudo sysctl -p /etc/sysctl.d/99-ipv4-forwarding.conf
+
+sysctl -w net.ipv4.tcp_window_scaling=1
+
+sysctl -w net.core.rmem_max=2097152
+
+sysctl -w net.core.wmem_max=2097152
+
+sysctl -w net.ipv4.tcp_rmem="4096 87380 2097152"
+
+sysctl -w net.ipv4.tcp_wmem="4096 65536 2097152"
 
 echo "Verifying installations..."
 birdc show status
