86547be581397ec81b2687d016e4ec0f48793c81
valornode <support@valornode.net>
2025-03-07 12:08:53 -0600

    Update install-basics.sh

--- install-basics.sh
+++ install-basics.sh
@@ -24,10 +24,9 @@ echo "Removing unneeded packages..."
 apt-get autoremove
 
 echo "Installing Pathvector..."
-curl -fsSL https://deb.pathvector.io/key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/pathvector-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/pathvector-archive-keyring.gpg] https://deb.pathvector.io stable main" | sudo tee /etc/apt/sources.list.d/pathvector.list > /dev/null
-sudo apt update -y
-sudo apt install -y pathvector
+curl https://repo.pathvector.io/pgp.asc > /usr/share/keyrings/pathvector.asc
+echo "deb [signed-by=/usr/share/keyrings/pathvector.asc] https://repo.pathvector.io/apt/ stable main" > /etc/apt/sources.list.d/pathvector.list
+apt update && apt install -y pathvector
 
 echo "Enabling IPv4 forwarding..."
 echo "net.ipv4.ip_forward=1" | sudo tee /etc/sysctl.d/99-ipv4-forwarding.conf > /dev/null
